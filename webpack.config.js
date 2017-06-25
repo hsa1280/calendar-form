0fd0d39c809850bec44e02d21c50eea22d8b86ce
Shian Huang <shian.huang@manheim.com>
2017-06-24 20:30:44 -0700

    added resolverloader

--- webpack.config.js
+++ webpack.config.js
@@ -15,8 +15,10 @@
       }
     }]
   },
-  resolve: {
-    extensions: [ '', '.js', '.jsx' ]
+  resolveLoader: {
+      modulesDirectories: ["web_loaders", "web_modules", "node_loaders", "node_modules"],
+      extensions: ["", ".webpack-loader.js", ".web-loader.js", ".loader.js", ".js"],
+      packageMains: ["webpackLoader", "webLoader", "loader", "main"]
   },
   devtool: "#inline-source-map"
 };
